33b2de6a6c320ced205aa3d9065c455eb97bb54c
卯月兔 <102902827+angel12538@users.noreply.github.com>
2026-08-24 15:05:08 +0800

    Update Constants.cs

--- DeepSeekTranslate/Modules/Constants.cs
+++ DeepSeekTranslate/Modules/Constants.cs
@@ -5,10 +5,11 @@ namespace DeepSeekTranslate
 {
     public partial class DeepSeekTranslateEndpoint : ITranslateEndpoint
     {
-        private static readonly HashSet<string> s_supportedSrcLangs = new HashSet<string> { "en", "ja", "ko", "ru", "zh", "zh-TW" };
-        private static readonly HashSet<string> s_supportedDstLangs = new HashSet<string> { "en", "ja", "ko", "ru", "zh", "zh-TW" };
+        private static readonly HashSet<string> s_supportedSrcLangs = new HashSet<string> { "auto","en", "ja", "ko", "ru", "zh", "zh-TW" };
+        private static readonly HashSet<string> s_supportedDstLangs = new HashSet<string> { "auto","en", "ja", "ko", "ru", "zh", "zh-TW" };
         private static readonly Dictionary<string, string> s_langZhDict = new Dictionary<string, string>
         {
+            { "auto", "自动识别语言" },
             { "en", "英语" },
             { "ja", "日语" },
             { "ko", "韩语" },
@@ -18,6 +19,7 @@ namespace DeepSeekTranslate
         };
         private static readonly Dictionary<string, string> s_langShortZhDict = new Dictionary<string, string>
         {
+            { "auto", "多语" },
             { "en", "英" },
             { "ja", "日" },
             { "ko", "韩" },
@@ -27,6 +29,7 @@ namespace DeepSeekTranslate
         };
         private static readonly Dictionary<string, string> s_trExampleDict = new Dictionary<string, string>
         {
+            { "auto", "愛は魂の深淵にある炎で,warm and never extinguished." },
             { "en", "Love is the flame in the depth of the soul, warm and never extinguished." },
             { "ja", "愛は魂の深淵にある炎で、暖かくて永遠に消えない。" },
             { "ko", "사랑은 영혼 깊숙이 타오르는 불꽃이며, 따뜻하고 영원히 꺼지지 않는다." },
